097f240e23f43b0ab12dd75ac7d2e72f9f570a31
york <york11122@gmail.com>
2022-08-18 19:12:48 +0800

    Add Chartjs

--- src/pages/expansion-menu/ExpansionMenu.vue
+++ src/pages/expansion-menu/ExpansionMenu.vue
@@ -1,11 +1,12 @@
 <template>
   <base-content>
-    ExpantionMenu
+    <bar-chart />
   </base-content>
 </template>
 
 <script lang="ts" setup>
 import BaseContent from 'src/components/BaseContent/BaseContent.vue';
+import BarChart from 'src/components/Charts/BarChart.vue';
 
 defineOptions({ name: "ExpantionMenu" })
 
