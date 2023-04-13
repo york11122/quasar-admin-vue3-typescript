1eaaa6093b760c98b74bf7e37c34f13b9fdb9079
york <york11122@gmail.com>
2023-04-13 20:32:04 +0800

    Add Toast UI editor

---
+++ src/pages/markdown/MDEditor copy.vue
@@ -0,0 +1,25 @@
+<template>
+    <base-content>
+        <markdown v-model="data" @save="saveArticle" />
+    </base-content>
+</template>
+
+
+<script lang="ts" setup>
+import Markdown from "src/components/Markdown/Markdown.vue";
+import BaseContent from "src/components/BaseContent/BaseContent.vue";
+import { useFetch } from "src/composables/fetch"
+import Notify from "src/components/Notify/Notify"
+
+defineOptions({ name: "MDEditor" })
+
+const { data } = useFetch("data/md-editor-v3.md").text()
+
+const saveArticle = () => {
+    Notify.create({
+        message: "Saved!",
+        color: "positive",
+    })
+}
+
+</script>
--- src/pages/markdown/MDEditor.vue
+++ src/pages/markdown/MDEditor.vue
@@ -1,12 +1,12 @@
 <template>
     <base-content>
-        <markdown v-model="data" @save="saveArticle" />
+        <markdown-toast />
     </base-content>
 </template>
 
 
 <script lang="ts" setup>
-import Markdown from "src/components/Markdown/Markdown.vue";
+import MarkdownToast from "src/components/Markdown/MarkdownToast.vue";
 import BaseContent from "src/components/BaseContent/BaseContent.vue";
 import { useFetch } from "src/composables/fetch"
 import Notify from "src/components/Notify/Notify"
