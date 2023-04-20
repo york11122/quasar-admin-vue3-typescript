9c31694f851007510aaa156d55de10c9808bd2c0
york <york11122@gmail.com>
2023-04-20 12:57:25 +0800

    Updates

---
+++ assets/ExpansionMenu.4c2a88df.js
@@ -0,0 +1,1 @@
+import{B as e}from"./BaseContent.2751e455.js";import{k as t,m as o,n,Q as a,W as r}from"./index.91d82a25.js";import"./QScrollArea.fd2d20e9.js";import"./selection.cb54467b.js";const s=t({name:"ExpantionMenu"}),l=t({...s,setup(c){return(p,_)=>(o(),n(e,null,{default:a(()=>[r(" on structure ")]),_:1}))}});export{l as default};
